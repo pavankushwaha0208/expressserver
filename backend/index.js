c1e9adba99ee47e91c5e9cdb6fb1e1a507cca8f7
pavan kushwaha <pavankush0208@gmail.com>
2023-10-06 11:53:34 +0530

    qw

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,47 @@
 import express from "express";
-
+import dotenv from 'dotenv'
 const app = express();
+dotenv.config()
 
 // app.get("/", (req, res) => {
 //   res.send("server is ready");
 // });
 
 //get a list of 5 jokes
+const jokes = [
+  {
+      id : 1,
+      title : 'A joke',
+      Content : 'This a joke'
+  },
+  {
+      id : 2,
+      title : 'Another joke',
+      Content : 'This a another joke'
+  },
+  {
+      id : 3,
+      title : 'A third',
+      Content : 'This a third joke'
+  },
+  {
+      id : 4,
+      title : 'A four',
+      Content : 'This a four joke'
+  },
+  {
+      id : 5,
+      title : 'A five',
+      Content : 'This a five pavan joke'
+  }
+]
 app.get('/api/jokes' , (req,res) => {
-  const jokes = [
-    {
-        id : 1,
-        title : 'A joke',
-        Content : 'This a joke'
-    },
-    {
-        id : 2,
-        title : 'Another joke',
-        Content : 'This a another joke'
-    },
-    {
-        id : 3,
-        title : 'A third',
-        Content : 'This a third joke'
-    },
-    {
-        id : 4,
-        title : 'A four',
-        Content : 'This a four joke'
-    },
-    {
-        id : 5,
-        title : 'A five',
-        Content : 'This a five joke'
-    }
-  ]
   res.send(jokes)
 })
 
 //port
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`server at http://localhost:${port}`);
 });
